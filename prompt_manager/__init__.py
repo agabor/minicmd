13e9de80fe5210a76abf582f85f77f9b2821ee43
Gabor Angyal <gabor.angyal@codesharp.dev>
2025-08-11 08:02:02 +0200

    refactor

--- prompt_manager/__init__.py
+++ prompt_manager/__init__.py
@@ -1,6 +1,6 @@
-from .editor import edit_prompt_file
-from .attachments import add_file_to_prompt
-from .attachment_reader import get_attachments
-from .reader import get_prompt_from_file
+from .edit_prompt_file import edit_prompt_file
+from .add_file_to_prompt import add_file_to_prompt
+from .get_attachments import get_attachments
+from .get_prompt_from_file import get_prompt_from_file
 
 __all__ = ['edit_prompt_file', 'add_file_to_prompt', 'get_attachments', 'get_prompt_from_file']
