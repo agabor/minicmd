5b3d5512ec4f5d3c378c4cbabbc61ff516176e53
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-02-17 14:45:56 +0100

    refactor block parsing

--- logic/fileprocessor.go
+++ logic/fileprocessor.go
@@ -6,29 +6,26 @@ import (
 
 func ParseCodeBlocks(response string) []CodeBlock {
 	lines := strings.Split(response, "\n")
-	inCodeBlock := false
 	var currentBlock *CodeBlock
 	var codeBlocks = make([]CodeBlock, 0)
 
 	for _, line := range lines {
 		if strings.HasPrefix(strings.TrimSpace(line), "````") {
-			if inCodeBlock {
+			if currentBlock != nil {
 				if len(currentBlock.lines) > 0 {
 					codeBlocks = append(codeBlocks, *currentBlock)
 				}
-				inCodeBlock = false
 				currentBlock = nil
 			} else {
-				inCodeBlock = true
 				blockHeader := strings.TrimSpace(strings.Replace(line, "````", "", 1))
 				currentBlock = &CodeBlock{blockHeader: blockHeader}
 			}
-		} else if inCodeBlock {
+		} else if currentBlock != nil {
 			currentBlock.lines = append(currentBlock.lines, line)
 		}
 	}
 
-	if inCodeBlock && currentBlock != nil && len(currentBlock.lines) > 0 {
+	if currentBlock != nil && len(currentBlock.lines) > 0 {
 		codeBlocks = append(codeBlocks, *currentBlock)
 	}
 
